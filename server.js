1110fadef0aaf7f2cdecb4d9dfb8c13b79dc6d40
andresdiazv <adiaz616@fiu.edu>
2023-01-24 11:03:29 -0500

    first commit

---
+++ server.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const { pool } = require("./dbConfig");
+const bcrypt = require("bcrypt");
+const app = express();
+
+const PORT = process.env.PORT || 3000;
+
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", (req, res) => {
+  res.render("index");
+});
+
+app.get("/users/register", (req, res) => {
+  res.render("register");
+});
+
+app.get("/users/login", (req, res) => {
+  res.render("login");
+});
+
+app.get("/users/dashboard", (req, res) => {
+  res.render("dashboard", { user: "John" });
+});
+
+app.post("/users/register", async (req, res) => {
+  let { name, email, password, password2 } = req.body;
+
+  let errors = [];
+
+  console.log({
+    name,
+    email,
+    password,
+    password2,
+  });
+
+  if (!name || !email || !password || !password2) {
+    errors.push({ message: "Please enter all fields" });
+  }
+
+  if (password.length < 6) {
+    errors.push({ message: "Password must be a least 6 characters long" });
+  }
+
+  if (password !== password2) {
+    errors.push({ message: "Passwords do not match" });
+  }
+
+  if (errors.length > 0) {
+    res.render("register", { errors, name, email, password, password2 });
+  } else {
+    hashedPassword = await bcrypt.hash(password, 10);
+    console.log(hashedPassword);
+    // Validation passed
+    pool.query(
+      `SELECT * FROM users
+          WHERE email = $1`,
+      [email],
+      (err, results) => {
+        if (err) {
+          console.log(err);
+        }
+        console.log(results.rows);
+
+        if (results.rows.length > 0) {
+          return res.render("register", {
+            message: "Email already registered",
+          });
+        } else {
+          pool.query(
+            `INSERT INTO users (name, email, password)
+                      VALUES ($1, $2, $3)
+                      RETURNING id, password`,
+            [name, email, hashedPassword],
+            (err, results) => {
+              if (err) {
+                throw err;
+              }
+              console.log(results.rows);
+              req.flash("success_msg", "You are now registered. Please log in");
+              res.redirect("/users/login");
+            }
+          );
+        }
+      }
+    );
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
